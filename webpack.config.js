93a2191a567f5c41b0be41c0b82c80200246a689
Zlepper <hansen13579@gmail.com>
2017-05-07 19:44:45 +0200

    Compact solution

--- webpack.config.js
+++ webpack.config.js
@@ -41,30 +41,9 @@ module.exports = {
                         }
                     }
                 ],
-                exclude: /(?:node_modules|worker\.ts)/,
-            },
-
-            {
-                // For our dear webworkers
-                test: /\.worker\.ts?$/,
-                use: [
-                    // {
-                    //     loader: 'worker-loader'
-                    // },
-                    {
-                        loader: 'awesome-typescript-loader',
-                        options: {
-                            configFileName: 'tsconfig.worker.json',
-
-                        }
-                    }
-                ],
-                exclude: /(?:node_modules)/
+                exclude: /(?:node_modules)/,
             },
         ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
     },
     devtool: 'inline-source-map'
 };
